03cbd9906b0aa575be32ba57343af46e18827747
sid palas <sid.palas@gmail.com>
2020-08-26 11:44:44 -0400

    Improved compose config

--- client/src/components/Links.jsx
+++ client/src/components/Links.jsx
@@ -19,7 +19,7 @@ class Links extends Component {
         return (
             <React.Fragment>
                 <Link to="/" className="navbar-brand">
-                    My first MERN Application
+                    Simple MERN Application
                 </Link>
                 <Collapse>
                     <List>
--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -10,8 +10,8 @@ const Wrapper = styled.a.attrs({
 class Logo extends Component {
     render() {
         return (
-            <Wrapper href="https://sambarros.com">
-                <img src={logo} width="50" height="50" alt="sambarros.com" />
+            <Wrapper>
+                <img src={logo} width="50" height="50" />
             </Wrapper>
         )
     }
